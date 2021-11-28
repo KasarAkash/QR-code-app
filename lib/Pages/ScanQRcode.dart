c4e24ac2280a0315647894bc966db7c39324e86f
kasar_akash <technicalak282@gmail.com>
2021-11-28 11:17:45 +0530

    Adding dependencies

--- lib/Pages/ScanQRcode.dart
+++ lib/Pages/ScanQRcode.dart
@@ -1,6 +1,9 @@
-// ignore_for_file: prefer_const_constructors, file_names
+// ignore_for_file: prefer_const_constructors, file_names, prefer_const_literals_to_create_immutables, deprecated_member_use
+
+import 'dart:io';
 
 import 'package:flutter/material.dart';
+import 'package:qr_code_scanner/qr_code_scanner.dart';
 
 class ScanQRcode extends StatefulWidget {
   const ScanQRcode({Key? key}) : super(key: key);
@@ -10,12 +13,65 @@ class ScanQRcode extends StatefulWidget {
 }
 
 class _ScanQRcodeState extends State<ScanQRcode> {
+  final qrKey = GlobalKey(debugLabel: 'QR');
+  QRViewController? controller;
+  Barcode? barcode;
+
+  @override
+  void dispose() {
+    super.dispose();
+    controller!.dispose();
+  }
+
+  @override
+  void reassemble() async {
+    super.reassemble();
+    if (Platform.isAndroid) {
+      await controller!.pauseCamera();
+    }
+    controller!.resumeCamera();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text("Scan QR Code"),
+      body: Stack(
+        alignment: Alignment.center,
+        children: [
+          buildQRview(context),
+          Positioned(
+            bottom: 10,
+            child: buidResult(),
+          ),
+        ],
       ),
+    );
+  }
+
+  Widget buidResult() => Text(
+        barcode != null ? '${barcode!.code}' : 'Scan a code!',
+        maxLines: 3,
+      );
+
+  Widget buildQRview(BuildContext context) => QRView(
+        key: qrKey,
+        onQRViewCreated: onQRViewCreated,
+        overlay: QrScannerOverlayShape(
+          borderWidth: 10,
+          borderLength: 22,
+          borderRadius: 12,
+          borderColor: Theme.of(context).accentColor,
+          cutOutSize: MediaQuery.of(context).size.width * 0.8,
+        ),
+      );
+
+  void onQRViewCreated(QRViewController controller) {
+    setState(() {
+      this.controller = controller;
+    });
+
+    controller.scannedDataStream.listen(
+      (barcode) => setState(() => this.barcode = barcode),
     );
   }
 }
